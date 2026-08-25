a6a8e5755039e8f6b2f6541149d44c6449626e42
rohannes220 <121151761+rohannes220@users.noreply.github.com>
2026-08-25 15:52:53 -0500

    Add database configuration

---
+++ app/db.py
@@ -0,0 +1,10 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import DeclarativeBase,sessionmaker
+from app.config import settings
+class Base(DeclarativeBase): pass
+engine=create_engine(settings.database_url)
+SessionLocal=sessionmaker(bind=engine,expire_on_commit=False)
+def get_db():
+    db=SessionLocal()
+    try: yield db
+    finally: db.close()
